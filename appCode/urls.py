5603b817b1a60a165484c400231b865389507978
SZJohanR4 <johan_szr@hotmail.com>
2019-05-18 08:57:11 -0500

    Update

--- appCode/urls.py
+++ appCode/urls.py
@@ -9,14 +9,12 @@ urlpatterns = [
     path('', views.index, name='index'),
 
     #conexiones paths
-    path('conexiones/', views.conexiones, name='conexiones'),
     path('conexiones/ethernet', views.conexiones, name='conexiones_ethernet'),
     path('conexiones/wifi', views.conexionesWifi, name='conexiones_wifi'),
     path('conexiones/clients', views.conexionesClients, name='conexiones_clients'),
     path('conexiones/gprs', views.conexionesGPRS, name='conexiones_gprs'),
 
     #red paths
-    path('red/', views.red, name='red'),
     path('red/sensor_inf', views.red, name='sensor_inf'),
     path('red/sensor_log', views.sensorLog, name='sensorLog'),
     path('red/sensor_mp', views.sensorMap, name='sensorMap'),
@@ -26,7 +24,6 @@ urlpatterns = [
     path('cloud_conector/', views.cloudConector, name='cloudConector'),
 
     #herrmaientas paths
-    path('herramientas/', views.herramientas, name='herramientas'),
     path('herramientas/ping', views.herramientas, name='herramientas'),
     path('herramientas/tracer', views.tracer, name='tracer'),
 
@@ -34,7 +31,6 @@ urlpatterns = [
     path('sistema/', views.sistema, name='sistema'),
 
     #data paths
-    path('data/', views.data, name='data'),
     path('data/get_data', views.data, name='get_data'),
     path('data/admin_tablas', views.adminTablas, name='adminTablas'),
     path('data/sync', views.sync, name='sync'),
--- static/js/navigationbars/navigationbars.js
+++ static/js/navigationbars/navigationbars.js
@@ -6,11 +6,12 @@ function currentPageActive() {
   // this function find the current path to add active class
   var path_sidebar = location.pathname;
   var path_navbar = "/"+location.pathname.split("/")[1]+"/"
+  navbar_element = document.getElementsByName(path_navbar)[0];
+  navbar_element.classList.add("active");
+  console.log(navbar_element);
+  console.log(path_sidebar+" ### "+path_navbar+ " ### "+ navbar_element);
    $('nav ul a').each(function() {
     if (this.pathname == path_sidebar) {
-     $(this).addClass('active');
-    }
-    if (this.pathname == path_navbar) {
      $(this).addClass('active');
     }
    });
